41685aa468079ae78cae29cb0b1a79859cb37abc
nicholas_fedetsky <nfedetsky@gmail.com>
2022-07-29 23:48:56 +0300

    commit 0.1

---
+++ src/exam_da.java
@@ -0,0 +1,9 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class exam_da {
+    public static void main(String[] args) {
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/yyyy/dd");
+        System.out.println(formatter.format(new Date()));
+    }
+}

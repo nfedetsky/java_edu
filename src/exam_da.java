420755658ee5dcfcd8c6d3da27ea17f79d7bb39b
nicholas_fedetsky <nfedetsky@gmail.com>
2022-07-31 13:34:45 +0300

    commit 2.1.1

--- src/exam_da.java
+++ src/exam_da.java
@@ -3,7 +3,7 @@ import java.util.Date;
 
 public class exam_da {
     public static void main(String[] args) {
-		System.out.println("Hello, guys!"
+		System.out.println("Hello, guys!")
         SimpleDateFormat formatter = new SimpleDateFormat("MM/yyyy/dd");
         System.out.println(formatter.format(new Date()));
     }

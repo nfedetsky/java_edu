05019eb227ae31e5468036074fff208d5699cdc9
nicholas_fedetsky <nfedetsky@gmail.com>
2022-07-31 14:12:37 +0300

    task FEDKLIS 3245

--- src/exam_da.java
+++ src/exam_da.java
@@ -4,7 +4,8 @@ import java.util.Date;
 public class exam_da {
     public static void main(String[] args) {
 		System.out.println("Hello, guys!")
-        SimpleDateFormat formatter = new SimpleDateFormat("MM/yyyy/dd");
+		System.out.println("Dzisiaj:")
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyyy");
         System.out.println(formatter.format(new Date()));
     }
 }
